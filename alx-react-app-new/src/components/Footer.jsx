d8b4246e6e93e79c933a330e5e11373068ff0651
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-07-18 05:51:18 +0300

    Addition of inline styles

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer style={{background:'navy',color:'white',textAlign:'center',padding:'10px'}}>
+            <p>@ 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
