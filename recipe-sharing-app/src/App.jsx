171939f50838787e4f174acc2966de2304f385ae
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-07-25 19:29:57 +0300

    Adding of Updation & Navigation

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,13 +1,29 @@
+import { Routes, Route } from "react-router-dom";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
     <div>
       <h1>Recipe Sharing App</h1>
-      <AddRecipeForm />
-      <hr />
-      <RecipeList />
+
+      <Routes>
+        {/*Homepage with form and list */}
+        <Route 
+        path="/"
+        element={
+          <div>
+            <AddRecipeForm />
+            <hr />
+            <RecipeList />
+          </div>
+        }
+        />
+        {/*Details page for individual recipes */}
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+      </Routes>
+      
     </div>
   );
 };
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import RecipeDetails from "./components/RecipeDetails";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />} />
-    <Route path="/recipe/:id" element={<RecipeDetails />} />
-  </Routes>
+  <App />
   
   </BrowserRouter>
 );
