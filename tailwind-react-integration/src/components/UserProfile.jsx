d755e07e9aedeabfa7671a9323b7ecc5b8d5bb65
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-08-08 06:01:54 +0300

    Styling the USerProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
             alt="User"
             className="rounded-full w-36 h-36 mx-auto"
             />
-            <h1 className="text-xl text-blue my-4">John Doe</h1>
+            <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
             <p className="text-base text-gray-600">
                 Developer at Example Co. Loves to write code and explore new technologies.
             </p>
