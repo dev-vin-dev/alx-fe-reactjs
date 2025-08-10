816ced1db16f2965e00a95c0d1bdd737fe028d92
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-08-10 12:00:04 +0300

    Adding Responsiveness

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
             <img 
             src="https://via.placeholder.com/150"
             alt="User"
-            className="rounded-full w-36 h-36 mx-auto md:w-36 md:h-36 object-cover mb-4"
+            className="rounded-full w-36 h-36 mx-auto md:w-36 md:h-36 object-cover mb-4 sm:w-24 sm:h-24 md:w-36"
             />
             <h1 className="text-xl text-blue-800 my-4 md:text-xl text-lg">John Doe</h1>
             <p className="text-base text-gray-600 text-sm md:text-base">
