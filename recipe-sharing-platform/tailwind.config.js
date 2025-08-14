0a4e017364e4939127d25724126aa3fb0a8c8589
dev-vin-dev <vincentmusyimi40@gmail.com>
2025-08-14 14:38:49 +0300

    Initializing the platform

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -3,7 +3,7 @@
 module.exports = { 
     content: [
         "./public/index.html",
-        ".src/**/*.{js,jsx,ts,tsx}",
+        "./src/**/*.{js,jsx,ts,tsx}",
     ],
     theme: {
         extend: {},
